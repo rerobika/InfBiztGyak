954e7b89d1691e467209974af82fa1961eac2e79
Robert Fancsik <frobert@inf.u-szeged.hu>
2019-03-14 07:43:55 +0100

    Gy06

---
+++ meet.c
@@ -0,0 +1,17 @@
+//meet.c
+// needed for screen printing
+#include <stdio.h>
+#include <string.h>
+greeting(char *temp1,char *temp2){ // greeting function to say hello
+char name[400];
+// string variable to hold the name
+strcpy(name, temp2);
+// copy the function argument to name
+printf("Hello %s %s\n", temp1, name); //print out the greeting
+}
+main(int argc, char * argv[]){
+//note the format for arguments
+greeting(argv[1], argv[2]);
+//call function, pass title & name
+printf("Bye %s %s\n", argv[1], argv[2]); //say "bye"
+} //exit program
